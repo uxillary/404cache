80f8003c6f6cecb0819306600ac175e7a7fe90a8
adamski <adamjch@proton.me>
2025-07-06 23:50:22 +0100

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="mb-4 text-center">
+      <h1 className="text-3xl glitch mb-1">404CACHE STOCK TERMINAL</h1>
+      <p className="text-green-500">Buy and sell the hottest fictional stocks!</p>
+    </header>
+  );
+}
+
+export default Header;
