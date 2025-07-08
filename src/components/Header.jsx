5ad88c5c779d8c678989dc0be61a2a75410dc4bb
adamski <adamjch@proton.me>
2025-07-08 13:57:32 +0100

    Improve styling with neon font and gradient

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="mb-4 text-center">
-      <h1 className="text-3xl glitch mb-1">404CACHE STOCK TERMINAL</h1>
+      <h1 className="text-4xl glitch mb-1 text-pink-400">404CACHE STOCK TERMINAL</h1>
       <p className="text-green-500">Buy and sell the hottest fictional stocks!</p>
     </header>
   );
